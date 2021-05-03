ad9498a9700d68535ed2b4e64da1e24fb5d358e8
mudakikwa <mudakikwastory@gmail.com>
2021-05-03 13:08:11 +0200

    added the recommendation history

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Python from './src/python.png';
 import CheckSquare from './src/check-square.svg';
-import Globe from './src/globe.svg';
+import ReactIcon from './src/react.svg';
+import Django from './src/django.svg';
+import boostrap from './src/boostrap.svg';
+import vue from './src/vue.svg';
+import ruby from './src/ruby.svg';
 
 import QuickFeutures from './quickFeutures/index';
 
@@ -12,15 +16,14 @@ export default function Body() {
     <div className="col-md-11" id="body">
       <h4 className="mx-0 px-0">Rendered codes</h4>
       <div className="row mx-0 px-0">
-        <div className="col-md-8 code-indicator px-0 ">
+        <div className="col-md-7 code-indicator px-0 ">
           <div className="row mx-0 code-card">
-            <div className="col-md-3 px-0 icon">
-              <div
-                className="coding-icon"
-                style={{ backgroundImage: `url("${Python}")` }}
-              />
+            <div className="col-md-2 px-0 icon">
+              <div className="coding-icon">
+                <img src={ReactIcon} alt="" />
+              </div>
             </div>
-            <div className="col-md-9 px-0">
+            <div className="col-md-10 px-0">
               <div className="row d-flex justify-content-between mx-0 px-0">
                 <div className="col-md-9 px-0">
                   <h5 className="title">creating a component using react</h5>
@@ -36,33 +39,26 @@ export default function Body() {
                   aliquyam erat, sed diam voluptua. At vero eos et accusam et
                   justo duo dolores et ea rebum. Stet clita kasd gubergren, no
                   sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                  ipsum dolor sit.
                 </h6>
               </div>
               <div className="row d-flex justify-content-between mx-0 px-0">
-                <div className="col-md-6 d-flex justify-content-start px-0">
-                  <img src={Globe} alt="" className="source" />
-                  <h6 className="source-website">
-                    tutorailpoint.com,w3schools
-                  </h6>
-                </div>
+                <div className="col-md-6 d-flex justify-content-start px-0"></div>
                 <div>
                   <h1 className="numbering">01</h1>
                 </div>
               </div>
             </div>
           </div>
-          <div className="row mx-0  code-card">
-            <div className="col-md-3 px-0 icon">
-              <div
-                className="coding-icon"
-                style={{ backgroundImage: `url("${Python}")` }}
-              />
+          <div className="row mx-0 code-card">
+            <div className="col-md-2 px-0 icon">
+              <div className="coding-icon">
+                <img src={Django} alt="" />
+              </div>
             </div>
-            <div className="col-md-9 px-0">
+            <div className="col-md-10 px-0">
               <div className="row d-flex justify-content-between mx-0 px-0">
                 <div className="col-md-9 px-0">
-                  <h5 className="title">creating a component using react</h5>
+                  <h5 className="title">Django Project Setup</h5>
                 </div>
                 <div className="col-md-3 px-0 d-flex justify-content-end">
                   <img src={CheckSquare} alt="" className="status" />
@@ -75,18 +71,108 @@ export default function Body() {
                   aliquyam erat, sed diam voluptua. At vero eos et accusam et
                   justo duo dolores et ea rebum. Stet clita kasd gubergren, no
                   sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                  ipsum dolor sit.
                 </h6>
               </div>
               <div className="row d-flex justify-content-between mx-0 px-0">
-                <div className="col-md-6 d-flex justify-content-start px-0">
-                  <img src={Globe} alt="" className="source" />
-                  <h6 className="source-website">
-                    tutorailpoint.com,w3schools
-                  </h6>
-                </div>
+                <div className="col-md-6 d-flex justify-content-start px-0"></div>
                 <div>
-                  <h1 className="numbering">01</h1>
+                  <h1 className="numbering">02</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mx-0 code-card">
+            <div className="col-md-2 px-0 icon">
+              <div className="coding-icon">
+                <img src={boostrap} alt="" />
+              </div>
+            </div>
+            <div className="col-md-10 px-0">
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-9 px-0">
+                  <h5 className="title">Boostrap Container</h5>
+                </div>
+                <div className="col-md-3 px-0 d-flex justify-content-end">
+                  <img src={CheckSquare} alt="" className="status" />
+                </div>
+              </div>
+              <div className="row mx-0 px-0">
+                <h6 className="px-0 mx-0 description">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                </h6>
+              </div>
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-6 d-flex justify-content-start px-0"></div>
+                <div>
+                  <h1 className="numbering">03</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mx-0 code-card">
+            <div className="col-md-2 px-0 icon">
+              <div className="coding-icon">
+                <img src={vue} alt="" />
+              </div>
+            </div>
+            <div className="col-md-10 px-0">
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-9 px-0">
+                  <h5 className="title">Vue Component Setup</h5>
+                </div>
+                <div className="col-md-3 px-0 d-flex justify-content-end">
+                  <img src={CheckSquare} alt="" className="status" />
+                </div>
+              </div>
+              <div className="row mx-0 px-0">
+                <h6 className="px-0 mx-0 description">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                </h6>
+              </div>
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-6 d-flex justify-content-start px-0"></div>
+                <div>
+                  <h1 className="numbering">04</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mx-0 code-card">
+            <div className="col-md-2 px-0 icon">
+              <div className="coding-icon">
+                <img src={ruby} alt="" />
+              </div>
+            </div>
+            <div className="col-md-10 px-0">
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-9 px-0">
+                  <h5 className="title">Ruby On Rails Put Request</h5>
+                </div>
+                <div className="col-md-3 px-0 d-flex justify-content-end">
+                  <img src={CheckSquare} alt="" className="status" />
+                </div>
+              </div>
+              <div className="row mx-0 px-0">
+                <h6 className="px-0 mx-0 description">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                </h6>
+              </div>
+              <div className="row d-flex justify-content-between mx-0 px-0">
+                <div className="col-md-6 d-flex justify-content-start px-0"></div>
+                <div>
+                  <h1 className="numbering">05</h1>
                 </div>
               </div>
             </div>
