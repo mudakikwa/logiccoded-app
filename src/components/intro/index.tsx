f9cb04d4f10b52f2e3a76bb72a8269665fb26445
mudakikwa <mudakikwastory@gmail.com>
2021-05-04 21:18:44 +0200

    finished the intro page

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./index.scss"
 
 export default function Intro() {
   return (
-    <div>
-
+    <div id="intro">
+      <h1>
+        Smart <span>coded .</span>{' '}
+      </h1>
     </div>
-  )
+  );
 }
 
